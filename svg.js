aab4ea4129282f76e9d6d1bde7b1c7fcc7f3dc6d
kchen36 <kchen36@stuy.edu>
2018-03-11 19:07:04 -0400

    done

--- svg.js
+++ svg.js
@@ -1,95 +1,66 @@
-var r = 25, x = 250, y = 250;
-var xplus = true;
-var yplus = true;
+var r = 25
 var frame = null;
-var c = null;
 var pic = document.getElementById("svg");
 var btn = document.getElementById("clear");
-var grow = document.getElementById('grow');
 
 var circle = function(e){
-    c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx", x + "");
-    c.setAttribute("cy", y + "");
+    clearInterval(frame);
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute("cx", e.offsetX + "");
+    c.setAttribute("cy", e.offsetY + "");
     c.setAttribute("r", r + "");
-    var 
-    c.createAttribute("xincrease");
-    c.createAttribute("yincrease");
-    
+    c.setAttribute("xplus", 1);
+    c.setAttribute("yplus", 1);
     c.setAttribute("fill", "black");
     c.setAttribute("stroke","black");
     pic.appendChild(c);    
 }
 
-var resize = function(){
-    if (r <= 0){
-	increase = true;
-    }
-    if (r >= 250){
-	increase = false;
-    }
-    if (increase){
-	r += 1;
-	c.setAttribute("r", r + "");
-    }
-    else{
-	r -= 1;
-	c.setAttribute("r", r + "");
-    }
-}
-
-var draw = function(e){
-    if (pic.hasChildNodes()){
-	clear();
-    }
-    circle();
-    frame = setInterval(resize, 10);
-}
-
-var bounce = function(){
-    if (pic.hasChildNodes()){
-	clear();
-    }
-    circle();
+var bounce = function(e){
+    circle(e);
     frame = setInterval(bouncehelper, 10);
 }
 var bouncehelper= function(){
-    if (x <= 25){
-	xplus = true;
+    var x,y,xplus,yplus;
+    for( i = 0; i < pic.children.length; i++){
+	c = pic.children[i];
+	x = Number(c.getAttribute("cx"));
+	y = Number(c.getAttribute("cy"));
+	xplus = Number(c.getAttribute("xplus"));
+	yplus = Number(c.getAttribute("yplus"));
+	if (x <= 25){
+	    c.setAttribute("xplus", 1);
+	}
+	if (x >= 475){
+	    c.setAttribute("xplus", 0);
+	}
+	if (y <= 25){
+	    c.setAttribute("yplus", 1);
+	}
+	if (y >= 480){
+	    c.setAttribute("yplus", 0);
+	}
+	if (xplus == 1){
+	    c.setAttribute("cx", x + 5);
+	}
+	else{
+	    c.setAttribute("cx", x - 5);
+	}
+	if (yplus == 1){
+	    c.setAttribute("cy", y + 5);
+	}
+	else{
+	    c.setAttribute("cy", y - 5);
+	}
     }
-    if (x >= 475){
-	xplus = false;
-    }
-    if (y <= 25){
-	yplus = true;
-    }
-    if (y >= 476){
-	yplus = false;
-    }
-    if (xplus){
-	x += 5;
-    }
-    else{
-	x -= 5;
-    }
-    if (yplus){
-	y += 5;
-    }
-    else{
-	y -= 5;
-    }
-    c.setAttribute("cx", x + "");
-    c.setAttribute("cy", y + "");
 }
 
 var clear = function() {
-    x = 250;
-    y = 250;
-    r = 25;
-    increase = true;
-    pic.removeChild(pic.firstChild);
-    clearInterval(frame)
+    while(pic.firstChild){
+	pic.removeChild(pic.firstChild);
+    }
+    clearInterval(frame);
 }
 
-bounceb.addEventListener("click",bounce);
+pic.addEventListener("click",bounce);
 btn.addEventListener("click",clear);
